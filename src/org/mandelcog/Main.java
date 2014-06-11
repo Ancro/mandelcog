eda2caa3f3895082546f35b671ab0300ba278b22
lopho <zweistecken@lopho.org>
2014-06-11 11:36:21 +0200

    window class stub

---
+++ src/org/mandelcog/Main.java
@@ -0,0 +1,11 @@
+package org.mandelcog;
+
+/**
+ * Created by lucas on 05.06.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Mandelcog app = new Mandelcog();
+        app.start();
+    }
+}
---
+++ src/org/mandelcog/Mandelcog.java
@@ -0,0 +1,19 @@
+package org.mandelcog;
+
+import org.mandelcog.ui.MandelWindow;
+
+/**
+ *
+ * @author  lopho
+ */
+public class Mandelcog {
+    private final MandelWindow window;
+    
+    public Mandelcog() {
+        window = new MandelWindow();
+    }
+    
+    public void start() {
+        
+    }
+}
